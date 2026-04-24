94ee0b65293305a2554ab5d996912c496b3f681d
Diego Ares <lafocacornio@gmail.com>
2026-04-24 20:47:10 +0200

    tal

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -9,7 +9,7 @@ public class Calculadora {
         Multiplicacion mult = new Multiplicacion();
         Division div = new Division();
 
-        System.out.println("Suma: " + suma.calcular(5, 4));
+        System.out.println("Suma A: " + suma.calcular(5, 4));
         System.out.println("Resta: " + resta.calcular(5, 3));
         System.out.println("Multiplicación: " + mult.calcular(5, 3));
         System.out.println("División: " + div.calcular(5, 3));
